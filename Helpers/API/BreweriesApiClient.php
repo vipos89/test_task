40db6c192b824011df0cdfe03e1ed5bc88e4cc02
vipos89 <webmaster.kobrin@gmail.com>
2020-04-14 11:05:30 +0300

    add libs register api wrapper in service provider

--- Helpers/API/BreweriesApiClient.php
+++ Helpers/API/BreweriesApiClient.php
@@ -2,12 +2,16 @@
 
 namespace App\Helpers\API;
 
+use GuzzleHttp\Client;
+
 class BreweriesApiClient
 {
     private $client;
 
     public function __construct()
     {
-
+        $this->client = new Client([
+           'base_uri'=>env('BREWERIES_API_BASE_URL', 'https://api.openbrewerydb.org/')
+        ]);
     }
 }
