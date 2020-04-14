dcb1db67b45c398e65e414f4c1cbba45f686cc3a
vipos89 <webmaster.kobrin@gmail.com>
2020-04-14 11:11:37 +0300

    update wrapper

--- Helpers/API/BreweriesApiClient.php
+++ Helpers/API/BreweriesApiClient.php
@@ -14,4 +14,22 @@ class BreweriesApiClient
            'base_uri'=>env('BREWERIES_API_BASE_URL', 'https://api.openbrewerydb.org/')
         ]);
     }
+
+    public function sendRequest($method, $uri, array $queryParams=[], array $formData = []){
+        try {
+            $resp = $this->client->request($method, $uri, [
+                'query'=>$queryParams,
+                'form_params'=>$formData
+            ]);
+            return [
+                'status'=>$resp->getStatusCode(),
+                'body'=>json_decode($resp->getBody()->getContents(), true)
+            ];
+        }catch (\Exception $exception){
+            return [
+                'status'=>'',
+                'body'=> $exception->getMessage()
+            ];
+        }
+    }
 }
